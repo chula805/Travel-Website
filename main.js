b8094698e09df2187b3e57d49eff7c4429611a3b
Chulani Vimukthi <121760253+chula805@users.noreply.github.com>
2023-11-14 16:48:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", scrollRevealOption);
+
+ScrollReveal().reveal(".header__container h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__container .section__header", scrollRevealOption);
+ScrollReveal().reveal(".about__container .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__container .about__flex", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".about__container .btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+// discover container
+ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card__content", {
+  ...scrollRevealOption,
+  interval: 500,
+  delay: 200,
+});
+
+// blogs container
+ScrollReveal().reveal(".blogs__card", {
+  duration: 1000,
+  interval: 400,
+});
+
+// journals container
+ScrollReveal().reveal(".journals__card", {
+  ...scrollRevealOption,
+  interval: 400,
+});
+
+// Adjust the height of the video based on the width of the viewport
+function adjustVideoSize() {
+  const videoContainers = document.querySelectorAll('.video__container');
+  videoContainers.forEach((container) => {
+    const width = container.offsetWidth;
+    const height = (width / 16) * 9; // 16:9 aspect ratio
+    container.style.height = `${height}px`;
+  });
+}
+
+// Call the function when the page loads and when it's resized
+window.addEventListener('load', adjustVideoSize);
+window.addEventListener('resize', adjustVideoSize);
+
+
